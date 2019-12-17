6e5652f53c0840ac5e6dc7e239c53777430a74b1
Meghan <meghan.e.mccue@gmail.com>
2019-12-17 14:31:08 +0000

    Step 13. Added station name and zone to Station class

--- lib/station.rb
+++ lib/station.rb
@@ -1,3 +1,9 @@
 class Station
+  attr_accessor :station_name, :zone
+
+  def initialize(station_name, zone)
+    @station_name = station_name
+    @zone = zone
+  end
 
 end 
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,6 +1,14 @@
-
 require 'station'
 
 describe Station do 
+  it 'stores name in name variable' do
+    station = Station.new("station1", 1)
+    expect(station.station_name).to eq "station1"
+  end
+
+  it 'stores zone in zone variable' do
+    station = Station.new("station1", 1)
+    expect(station.zone).to eq 1
+  end
 
 end 
